65cb2269a594b2a28953a396c1e8727d92ca9e31
Ernest Cui <ecuiships@126.com>
2023-04-19 23:44:29 +0800

    chore: remove react-intl things

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { useAppSelector } from "./store/hooks";
 import "antd/dist/reset.css";
-import { IntlProvider } from "react-intl";
 
 const App: React.FC = () => {
     const currentLocale = useAppSelector(state => state.locale.currentLocale);
 
     return (
-        <IntlProvider messages={currentLocale} locale="en">
-            <div className="App"></div>
-        </IntlProvider>
+        <div className="App"></div>
     );
 };
 
