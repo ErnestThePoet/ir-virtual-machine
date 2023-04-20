eaa3ab635ff321b587e0b7e900c63e9a53ec2f5d
Ernest Cui <ecuiships@126.com>
2023-04-20 15:38:25 +0800

    chore: add react-intl and router provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { useAppSelector } from "./store/hooks";
 import "antd/dist/reset.css";
+import { RouterProvider } from "react-router-dom";
+import { IntlProvider } from "react-intl";
+import router from "./router";
 
 const App: React.FC = () => {
     const currentLocale = useAppSelector(state => state.locale.currentLocale);
 
     return (
-        <div className="App"></div>
+        <IntlProvider messages={currentLocale} locale="en">
+            <RouterProvider router={router} />
+        </IntlProvider>
     );
 };
 
