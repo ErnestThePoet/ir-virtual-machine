0055373e4bc78668661a4483a7fbfb6a0f432048
Ernest Cui <ecuiships@126.com>
2023-04-20 23:54:30 +0800

    docs: updated Aint32 doc

--- src/modules/vm/data_types/aint32.ts
+++ src/modules/vm/data_types/aint32.ts
@@ -1,8 +1,8 @@
 type Int32Type = "INT32" | "UINT32";
 
 /**
- * The presence of signed and unsigned Int32 types 
- * makes value truncation automatic.
+ * The abstract base class of 32bit integer types Int32(signed) and Uint32(unsigned).
+ * These classes make value truncation automatic.
  * All subclasses are immutable.
  */
 export abstract class Aint32 {
