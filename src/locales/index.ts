6dc7c28e719e327a780a9fb9593fc76cab7e04b1
Ernest Cui <ecuiships@126.com>
2023-04-20 16:09:11 +0800

    chore: add FormattableMessage type

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -5,12 +5,16 @@ import zhCn from "./zhcn";
 
 export type AppLocale = typeof zhCn;
 export type AppLocaleKey = keyof typeof zhCn;
+export interface FormattableMessage {
+    key: AppLocaleKey;
+    values?: {};
+}
 
 const locales: { name: string; locale: AppLocale }[] = [
     {
         name: "简体中文",
         locale: zhCn
-    },
+    }
     // Add new language entry here //
 
     /////////////////////////////////
