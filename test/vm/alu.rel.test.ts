8eb3346d8b20dbe5a89e3eae3a117ce62e75bfb9
Ernest Cui <ecuiships@126.com>
2023-04-20 14:40:45 +0800

    test: separate ALU arithmetic and rel test

---
+++ test/vm/alu.rel.test.ts
@@ -0,0 +1,8 @@
+import { test, expect } from "@jest/globals";
+import { Uint32, Int32 } from "../../src/modules/vm/data_types";
+
+const alu = new Alu();
+
+test("(int32)1===(int32)1", () => {
+    expect(a)
+})
