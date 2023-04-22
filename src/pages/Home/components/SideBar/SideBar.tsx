7fa6d3ba00632fc20d442e2668bf5b0b676f8b58
Ernest Cui <ecuiships@126.com>
2023-04-22 20:58:59 +0800

    feat: add VM complete

--- src/pages/Home/components/SideBar/SideBar.tsx
+++ src/pages/Home/components/SideBar/SideBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import styles from "./SideBar.module.scss";
 import {
@@ -10,15 +10,44 @@ import {
     EyeOutlined
 } from "@ant-design/icons";
 import SideBarIcon from "./SideBarIcon";
+import vmContainer from "@/modules/vmContainer";
+import type { SingleVmPageState } from "@/store/reducers/vm";
+import { addVmPageState, setActiveVmIndex } from "@/store/reducers/vm";
+import { Vm } from "@/modules/vm/vm";
+
+function getNextUntitledVmName(names: string[]): string {
+    for (let i = 1; ; i++) {
+        const currentName = `Untitled-${i}`;
+        if (names.every(x => x !== currentName)) {
+            return currentName;
+        }
+    }
+}
 
 const SideBar: React.FC = () => {
+    const dispatch = useAppDispatch();
+    const vm = useAppSelector(state => state.vm);
+
     return (
         <div className={styles.divSideBarWrapper}>
             <div className={styles.divIconWrapperUpper}>
                 <SideBarIcon
                     icon={<FileAddOutlined />}
                     label="新建"
-                    onClick={() => {}}
+                    onClick={() => {
+                        const vmName = getNextUntitledVmName(
+                            vm.vmPageStates.map(x => x.name)
+                        );
+
+                        vmContainer.add(new Vm());
+                        dispatch(
+                            addVmPageState({
+                                name: vmName,
+                                irPath: "",
+                                isIrChanged: false
+                            } as SingleVmPageState)
+                        );
+                    }}
                 />
                 <SideBarIcon
                     icon={<FolderOpenOutlined />}
