fe29470532225ce57509de67d5f1b28d58e37285
Ernest Cui <ecuiships@126.com>
2023-04-22 20:25:34 +0800

    chore: modify vm reducer to contain no Vm instance

--- src/pages/Home/components/TabBar/TabBar.tsx
+++ src/pages/Home/components/TabBar/TabBar.tsx
@@ -1,19 +1,21 @@
 import React from "react";
-import { useAppSelector } from "@/store/hooks";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import styles from "./TabBar.module.scss";
 import TabBarItem from "./TabBarItem";
+import { switchActiveVm } from "@/store/reducers/vm";
 
 const TabBar: React.FC = () => {
     const vm = useAppSelector(state => state.vm);
+    const dispatch = useAppDispatch();
 
     return (
         <div className={styles.divTabBarWrapper}>
-            {new Array(10).fill("杨丛语杨丛语杨丛语").map((x, i) => (
+            {vm.vmPageStates.map((x, i) => (
                 <TabBarItem
-                    title={x.toString()}
-                    isActive={i % 3 === 0}
-                    isChanged={i % 4 === 0}
-                    onClick={() => {}}
+                    title={x.name}
+                    isActive={i === vm.activeVmIndex}
+                    isChanged={x.isIrChanged}
+                    onClick={() => dispatch(switchActiveVm(i))}
                     onCloseClick={() => {}}
                 />
             ))}
--- src/store/reducers/vm/vm.ts
+++ src/store/reducers/vm/vm.ts
@@ -1,36 +1,50 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { Vm } from "@/modules/vm/vm";
 
+interface SingleVmPageState {
+    name: string;
+    irPath: string;
+    isIrChanged: boolean;
+}
+
 interface VmState {
-    vms: {
-        name: string;
-        irPath: string;
-        vm: Vm;
-    }[];
-    currentVmIndex: number;
+    vmPageStates: SingleVmPageState[];
+    activeVmIndex: number;
 }
 
 const initialState: VmState = {
-    vms: [],
-    currentVmIndex: 0
+    vmPageStates: [],
+    activeVmIndex: 0
 };
 
 export const vmSlice = createSlice({
     name: "vm",
     initialState,
     reducers: {
-        setVms: (state, action) => {
-            state.vms = action.payload;
+        switchActiveVm: (state, action) => {
+            state.activeVmIndex = action.payload;
         },
-        switchVm: (state, action) => {
-            state.currentVmIndex = action.payload;
+        setVmPageState: (state, action) => {
+            state.vmPageStates[action.payload.index] = action.payload.state;
         },
-        updateVm: (state, action) => {
-            state.vms[state.currentVmIndex].vm = action.payload;
+        addVmPageState: (state, action) => {
+            state.vmPageStates.push(action.payload);
+            state.activeVmIndex = state.vmPageStates.length - 1;
+        },
+        deleteVmPageState: (state, action) => {
+            if (state.activeVmIndex >= action.payload.index) {
+                state.activeVmIndex--;
+            }
+            state.vmPageStates.splice(action.payload.index, 1);
         }
     }
 });
 
-export const { setVms, switchVm, updateVm } = vmSlice.actions;
+export const {
+    switchActiveVm,
+    setVmPageState,
+    addVmPageState,
+    deleteVmPageState
+} = vmSlice.actions;
 
 export default vmSlice.reducer;
