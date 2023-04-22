5434802049849bfa64685943c6bda2810044435e
Ernest Cui <ecuiships@126.com>
2023-04-22 13:28:09 +0800

    chore: sidabar complete

--- src/pages/Home/components/SideBar/SideBar.tsx
+++ src/pages/Home/components/SideBar/SideBar.tsx
@@ -1,14 +1,38 @@
 import React, { useState } from "react";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import styles from "./SideBar.module.scss";
-import { FileAddOutlined } from "@ant-design/icons";
+import {
+    FileAddOutlined,
+    FolderOpenOutlined,
+    SaveOutlined,
+    InfoCircleOutlined
+} from "@ant-design/icons";
+import SideBarIcon from "./SideBarIcon";
 
 const SideBar: React.FC = () => {
-    const theme = useAppSelector(state => state.theme.currentTheme);
-
     return (
         <div className={styles.divSideBarWrapper}>
-            <FileAddOutlined />
+            <SideBarIcon
+                icon={<FileAddOutlined />}
+                label="新建"
+                onClick={() => {}}
+            />
+            <SideBarIcon
+                icon={<FolderOpenOutlined />}
+                label="导入"
+                onClick={() => {}}
+            />
+            <SideBarIcon
+                icon={<SaveOutlined />}
+                label="保存"
+                onClick={() => {}}
+            />
+
+            <SideBarIcon
+                icon={<InfoCircleOutlined />}
+                label="关于"
+                onClick={() => {}}
+            />
         </div>
     );
 };
---
+++ src/pages/Home/components/SideBar/SideBarIcon/SideBarIcon.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./SideBarIcon.module.scss";
+
+interface SideBarIconProps {
+    icon: React.ReactNode;
+    label?: string;
+    onClick: () => void;
+}
+
+const SideBarIcon: React.FC<SideBarIconProps> = (props: SideBarIconProps) => {
+    return (
+        <div className={styles.divIconWrapper} onClick={props.onClick}>
+            {props.icon}
+            {props.label !== undefined && <label>{props.label}</label>}
+        </div>
+    );
+};
+
+export default SideBarIcon;
