3a1c992c80c12b0c223fbe7478c87810bb12d68d
Ernest Cui <ecuiships@126.com>
2023-04-22 13:42:57 +0800

    chore: add lang and themes to sidebar

--- src/pages/Home/components/SideBar/SideBar.tsx
+++ src/pages/Home/components/SideBar/SideBar.tsx
@@ -5,34 +5,50 @@ import {
     FileAddOutlined,
     FolderOpenOutlined,
     SaveOutlined,
-    InfoCircleOutlined
+    InfoCircleOutlined,
+    TranslationOutlined,
+    EyeOutlined
 } from "@ant-design/icons";
 import SideBarIcon from "./SideBarIcon";
 
 const SideBar: React.FC = () => {
     return (
         <div className={styles.divSideBarWrapper}>
-            <SideBarIcon
-                icon={<FileAddOutlined />}
-                label="新建"
-                onClick={() => {}}
-            />
-            <SideBarIcon
-                icon={<FolderOpenOutlined />}
-                label="导入"
-                onClick={() => {}}
-            />
-            <SideBarIcon
-                icon={<SaveOutlined />}
-                label="保存"
-                onClick={() => {}}
-            />
+            <div className={styles.divIconWrapperUpper}>
+                <SideBarIcon
+                    icon={<FileAddOutlined />}
+                    label="新建"
+                    onClick={() => {}}
+                />
+                <SideBarIcon
+                    icon={<FolderOpenOutlined />}
+                    label="导入"
+                    onClick={() => {}}
+                />
+                <SideBarIcon
+                    icon={<SaveOutlined />}
+                    label="保存"
+                    onClick={() => {}}
+                />
+            </div>
 
-            <SideBarIcon
-                icon={<InfoCircleOutlined />}
-                label="关于"
-                onClick={() => {}}
-            />
+            <div className={styles.divIconWrapperLower}>
+                <SideBarIcon
+                    icon={<TranslationOutlined />}
+                    label="Lang"
+                    onClick={() => {}}
+                />
+                <SideBarIcon
+                    icon={<EyeOutlined />}
+                    label="主题"
+                    onClick={() => {}}
+                />
+                <SideBarIcon
+                    icon={<InfoCircleOutlined />}
+                    label="关于"
+                    onClick={() => {}}
+                />
+            </div>
         </div>
     );
 };
