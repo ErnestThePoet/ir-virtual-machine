d28df265167462db361ea58b5d35172ad9500fc4
Ernest Cui <ecuiships@126.com>
2023-04-22 19:41:53 +0800

    chore: add truncateString util

--- src/modules/utils.ts
+++ src/modules/utils.ts
@@ -3,3 +3,25 @@ import { Uint32 } from "./vm/data_types";
 export function toHex(value: Uint32): string {
     return `0x${value.value.toString(16).toUpperCase()}`;
 }
+
+export function truncateString(title: string, length: number = 10) {
+    let stringLengthAtDisplayLength = 0;
+    let displayLength = 0;
+    for (let i = 0; i < title.length; i++) {
+        if (title[i].match(/^[\x00-\x7F]*$/) !== null) {
+            displayLength++;
+        } else {
+            displayLength += 2;
+        }
+
+        if (displayLength > length) {
+            stringLengthAtDisplayLength = i;
+            break;
+        }
+    }
+
+    if (displayLength <= length) {
+        return title;
+    }
+    return title.substring(0, stringLengthAtDisplayLength) + "...";
+}
