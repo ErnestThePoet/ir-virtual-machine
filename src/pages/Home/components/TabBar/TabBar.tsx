1c3f2a8de27e273bc46731004e67d9f4a5e65d2c
Ernest Cui <ecuiships@126.com>
2023-04-22 20:53:33 +0800

    chore: change import name to setActiveVmIndex

--- src/pages/Home/components/TabBar/TabBar.tsx
+++ src/pages/Home/components/TabBar/TabBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import styles from "./TabBar.module.scss";
 import TabBarItem from "./TabBarItem";
-import { switchActiveVm } from "@/store/reducers/vm";
+import { setActiveVmIndex } from "@/store/reducers/vm";
 
 const TabBar: React.FC = () => {
     const vm = useAppSelector(state => state.vm);
@@ -15,7 +15,7 @@ const TabBar: React.FC = () => {
                     title={x.name}
                     isActive={i === vm.activeVmIndex}
                     isChanged={x.isIrChanged}
-                    onClick={() => dispatch(switchActiveVm(i))}
+                    onClick={() => dispatch(setActiveVmIndex(i))}
                     onCloseClick={() => {}}
                 />
             ))}
