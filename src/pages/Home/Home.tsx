c194df36de382c5bb45d6ac89a4c49d02302525b
Ernest Cui <ecuiships@126.com>
2023-04-23 12:03:08 +0800

    feat: auto fit screen size

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,30 +1,43 @@
-import React, { useState } from "react";
-import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import React, { useState, useEffect } from "react";
+import { useAppSelector } from "@/store/hooks";
 import styles from "./Home.module.scss";
 import SideBar from "./components/SideBar";
 import TabBar from "./components/TabBar";
 import EmptyHolder from "./components/EmptyHolder/EmptyHolder";
 import IrEditor from "./components/IrEditor/IrEditor";
+import classNames from "classnames";
 
 const Home: React.FC = () => {
     const vmStates = useAppSelector(state => state.vm.vmPageStates);
+
+    const [isVerticalScreen, setIsVerticalScreen] = useState(
+        window.innerWidth < window.innerHeight
+    );
+
+    useEffect(() => {
+        window.onresize = () =>
+            setIsVerticalScreen(window.innerWidth < window.innerHeight);
+    }, []);
 
     return (
         <main className={styles.main}>
             <SideBar />
             <div className={styles.divRight}>
                 <TabBar />
-                <div className={styles.divVmWrapper}>
+                <div
+                    className={classNames({
+                        [styles.divVmWrapperHorizontal]: !isVerticalScreen,
+                        [styles.divVmWrapperVertical]: isVerticalScreen
+                    })}>
                     {vmStates.length === 0 ? (
                         <EmptyHolder />
                     ) : (
                         <>
-                            <section className={styles.sectionIr}>
+                            <section className="sectionIr">
                                 <IrEditor />
                             </section>
 
-                            <section
-                                className={styles.sectionConsole}></section>
+                            <section className="sectionConsole"></section>
                         </>
                     )}
                 </div>
