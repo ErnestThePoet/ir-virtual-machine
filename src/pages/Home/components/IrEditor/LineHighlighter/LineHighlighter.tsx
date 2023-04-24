5c98a5df2f668dbb74b8796d6936e5db72c09466
Ernest Cui <ecuiships@126.com>
2023-04-24 08:39:24 +0800

    feat: add LineHighlighter

---
+++ src/pages/Home/components/IrEditor/LineHighlighter/LineHighlighter.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./LineHighlighter.module.scss";
+import classNames from "classnames";
+
+interface LineHighlighterProps {
+    type: "ERROR" | "INDICATION";
+}
+
+const LineHighlighter: React.FC<LineHighlighterProps> = (
+    props: LineHighlighterProps
+) => {
+    return (
+        <div className={styles.divLineHighlighterWrapper}>
+            <div
+                className={classNames({
+                    [styles.divLineFrameError]: props.type === "ERROR",
+                    [styles.divLineFrameIndication]: props.type === "INDICATION"
+                })}></div>
+        </div>
+    );
+};
+
+export default LineHighlighter;
