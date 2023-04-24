0dcf287947b2e637b22ca60262be5d686395feb5
Ernest Cui <ecuiships@126.com>
2023-04-24 22:47:28 +0800

    chore: remove :

--- src/locales/zhcn.ts
+++ src/locales/zhcn.ts
@@ -36,9 +36,9 @@ const zhCn = {
     MEMORY_SIZE: "内存大小/B",
     STACK_SIZE: "栈大小/B",
 
-    TOTAL_MEMORY_USAGE: "总内存使用：",
-    STACK_MEMORY_USAGE: "栈内存使用：",
-    GLOBAL_VARIABLE_MEMORY_USAGE: "全局变量内存使用：",
+    TOTAL_MEMORY_USAGE: "总内存使用",
+    STACK_MEMORY_USAGE: "栈内存使用",
+    GLOBAL_VARIABLE_MEMORY_USAGE: "全局变量内存使用",
     PERCENTAGE_USAGE: "{percentage, number, ::.0}%",
     B_USAGE: "{used}B/{total}B",
     KB_USAGE: "{used, number, ::.0}KB/{total, number, ::.0}KB",
