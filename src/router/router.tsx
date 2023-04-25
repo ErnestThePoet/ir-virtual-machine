1580670c44d3e81a1257ce260368d07f26487f85
Ernest Cui <ecuiships@126.com>
2023-04-25 13:23:09 +0800

    bug: add router basename

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,11 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 
-const customRouter = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />
-    }
-]);
+const customRouter = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <Home />
+        }
+    ],
+    { basename: "/IR-Virtual-Machine" }
+);
 
 export default customRouter;
