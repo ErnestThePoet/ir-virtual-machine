e713e836bae1696722c720f3124f203359e9056e
Ernest Cui <ecuiships@126.com>
2023-05-06 15:34:38 +0800

    bug: unchanged tabbar item onClose binds to div

--- src/pages/Home/components/TabBar/TabBarItem/TabBarItem.tsx
+++ src/pages/Home/components/TabBar/TabBarItem/TabBarItem.tsx
@@ -76,25 +76,23 @@ const TabBarItem: React.FC<TabBarItemProps> = (props: TabBarItemProps) => {
             {props.isChanged ? (
                 <div
                     className={styles.divCloseWrapperChanged}
+                    title={intl.formatMessage({ id: "CLOSE" })}
                     onClick={e => {
                         e.stopPropagation();
                         props.onCloseClick();
-                    }}
-                    title={intl.formatMessage({ id: "CLOSE" })}>
+                    }}>
                     <Dot className={styles.iconDot} />
                     <CloseOutlined className={styles.iconClose} />
                 </div>
             ) : (
                 <div
                     className={styles.divCloseWrapper}
-                    title={intl.formatMessage({ id: "CLOSE" })}>
-                    <CloseOutlined
-                        className={styles.iconClose}
-                        onClick={e => {
-                            e.stopPropagation();
-                            props.onCloseClick();
-                        }}
-                    />
+                    title={intl.formatMessage({ id: "CLOSE" })}
+                    onClick={e => {
+                        e.stopPropagation();
+                        props.onCloseClick();
+                    }}>
+                    <CloseOutlined className={styles.iconClose} />
                 </div>
             )}
         </div>
