5f1ba8b19fde5a5550acf5d00a2bd2b297f3be48
Ernest Cui <ecuiships@126.com>
2024-04-10 18:33:39 +0800

    chore: memorize variable table

--- src/pages/Home/components/VmInspector/VariableTable/VariableTable.tsx
+++ src/pages/Home/components/VmInspector/VariableTable/VariableTable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { memo } from "react";
 import styles from "./VariableTable.module.scss";
 import { useIntl } from "react-intl";
 import type { VmVariableDetail } from "@/modules/vm/vm";
@@ -10,68 +10,68 @@ interface VariableTableProps {
     variables: VmVariableDetail[];
 }
 
-const VariableTable: React.FC<VariableTableProps> = (
-    props: VariableTableProps
-) => {
-    const intl = useIntl();
+const VariableTable: React.FC<VariableTableProps> = memo(
+    (props: VariableTableProps) => {
+        const intl = useIntl();
 
-    const columns: ColumnsType<VmVariableDetail> = [
-        {
-            title: intl.formatMessage({ id: "VARIABLE_ID" }),
-            dataIndex: "id",
-            showSorterTooltip: false,
-            sorter: (a, b) => stringCompare(a.id, b.id)
-        },
-        {
-            title: intl.formatMessage({ id: "ADDRESS" }),
-            dataIndex: "address",
-            showSorterTooltip: false,
-            sorter: (a, b) => a.address - b.address
-        },
-        {
-            title: intl.formatMessage({ id: "SIZE" }),
-            dataIndex: "size",
-            showSorterTooltip: false,
-            sorter: (a, b) => a.size - b.size
-        },
-        {
-            title: intl.formatMessage({ id: "VALUES" }),
-            dataIndex: "values",
-            render: (x: number[]) => (
-                <div className={styles.divValues}>
-                    {x.map((y, i) => (
-                        <div key={`${i}${y}`}>
-                            {x.length === 1 ? (
-                                y
-                            ) : (
-                                <>
-                                    [{i}]: {y}
-                                </>
-                            )}
-                        </div>
-                    ))}
-                </div>
-            )
-        }
-    ];
+        const columns: ColumnsType<VmVariableDetail> = [
+            {
+                title: intl.formatMessage({ id: "VARIABLE_ID" }),
+                dataIndex: "id",
+                showSorterTooltip: false,
+                sorter: (a, b) => stringCompare(a.id, b.id)
+            },
+            {
+                title: intl.formatMessage({ id: "ADDRESS" }),
+                dataIndex: "address",
+                showSorterTooltip: false,
+                sorter: (a, b) => a.address - b.address
+            },
+            {
+                title: intl.formatMessage({ id: "SIZE" }),
+                dataIndex: "size",
+                showSorterTooltip: false,
+                sorter: (a, b) => a.size - b.size
+            },
+            {
+                title: intl.formatMessage({ id: "VALUES" }),
+                dataIndex: "values",
+                render: (x: number[]) => (
+                    <div className={styles.divValues}>
+                        {x.map((y, i) => (
+                            <div key={`${i}${y}`}>
+                                {x.length === 1 ? (
+                                    y
+                                ) : (
+                                    <>
+                                        [{i}]: {y}
+                                    </>
+                                )}
+                            </div>
+                        ))}
+                    </div>
+                )
+            }
+        ];
 
-    return (
-        <div className={styles.divVariableTableWrapper}>
-            {props.variables.length === 0 ? (
-                <div className="emptyHolder">
-                    {intl.formatMessage({ id: "EMPTY_VATIABLE_TABLE" })}
-                </div>
-            ) : (
-                <Table
-                    rowKey="id"
-                    columns={columns}
-                    dataSource={props.variables}
-                    size="small"
-                    pagination={false}
-                />
-            )}
-        </div>
-    );
-};
+        return (
+            <div className={styles.divVariableTableWrapper}>
+                {props.variables.length === 0 ? (
+                    <div className="emptyHolder">
+                        {intl.formatMessage({ id: "EMPTY_VATIABLE_TABLE" })}
+                    </div>
+                ) : (
+                    <Table
+                        rowKey="id"
+                        columns={columns}
+                        dataSource={props.variables}
+                        size="small"
+                        pagination={false}
+                    />
+                )}
+            </div>
+        );
+    }
+);
 
 export default VariableTable;
