258be081d7a78ac1c1e888a47be0ebe54b07ea28
Ernest Cui <ecuiships@126.com>
2024-04-24 23:53:37 +0800

    chore: default max step to 5M

--- src/modules/vm/vm.ts
+++ src/modules/vm/vm.ts
@@ -218,7 +218,7 @@ export const vmOptionLimits: {
 };
 
 const defaultOptions: VmOptions = {
-    maxExecutionStepCount: 1_000_000,
+    maxExecutionStepCount: 5_000_000,
     memorySize: 16 * 1024,
     stackSize: 8 * 1024
 };
